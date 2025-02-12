19c9c692a3adeeb6f6237affab1e3bdb9a0eba02
gax1985 <91019131+gax1985@users.noreply.github.com>
2025-02-11 22:38:19 -0400

    Created Ollama.py with Ollama_Python(Need to try the original Ollama library)

--- Python Files/Ollama.py
+++ Python Files/Ollama.py
@@ -1,5 +1,5 @@
 #Let us start by importing the much needed Ollama-Python library :
-
+import ollama
 import ollama_python
 
 from ollama_python.endpoints import ModelManagementAPI
@@ -7,7 +7,6 @@ from ollama_python.endpoints import ModelManagementAPI
 # We can declare the Ollama Model Management API's endpoint, so we can start with issuing commands to get the model :
 
 ModelManagerAPI = ollama_python.endpoints.ModelManagementAPI(base_url="http://localhost:11434/api")
-print(ModelManagerAPI.__getstate__())
 
 
 # ... next, let us create a function to pull the desired model :
@@ -16,10 +15,12 @@ def ollama_model_gogetter(modelname):
 
 #  ... let us use it to pull the model from the Ollama-Python library:
 
-    modelname = str(modelname)
-    TheGetter = ModelManagerAPI.pull(modelname)
-
-    # Debugging Statements
+    try:
+        TheGetter = ModelManagerAPI.pull(modelname)
+        print(f"Model {modelname} has been downloaded successfully!")
+    except Exception as e:
+        print(f"Error : {e}")
+# Debugging Statements
     print(TheGetter.status)
     ModelManagerAPI.list_local_models()
 
@@ -31,14 +32,23 @@ def ollama_model_gogetter(modelname):
 def the_ollama_generator(): #nmap_report,nikto_report,clamav_report,wapiti_report):
 
 #   What we need to do here is to use the GenerateAPI library, where we indicate Ollama's API Endpoint, and we select our LLM :
+    from ollama_python import endpoints
+    generation_api = ollama_python.endpoints.GenerateAPI(model="ALIENTELLIGENCE/cybersecuritythreatanalysisv2",base_url="http://127.0.0.1:11434/api")
 
-    generation_api = ollama_python. endpoints.GenerateAPI(base_url="http://127.0.0.1:11434/api",model="ALIENTELLIGENCE/cybersecuritythreatanalysisv2")
+#   Let us add a variable and store the system prompt in it :
 
+    # system_instruction = {
+    #         "role": "system",
+    #         "content": "You are a Cybersecurity Expert with many years of experience in Cybersecurity. You are a friendly professional and have an eye for detail. You are an expert at vulnerability assessment report generation where you convey the results of network vulnerability assessments, web application vulnerability assessments and malicious file scanning in a friendly, accurate and as non-technical as possible. We would like to be approachable to a wide variety of audiences, thus this is the reason behind this instruction."
+    #     }
+    system_instruction = "You are a Cybersecurity Expert with many years of experience in Cybersecurity. You are a friendly professional and have an eye for detail. You are an expert at vulnerability assessment report generation where you convey the results of network vulnerability assessments, web application vulnerability assessments and malicious file scanning in a friendly, accurate and as non-technical as possible. We would like to be approachable to a wide variety of audiences, thus this is the reason behind this instruction."
 #    messages = [{'role':'user','content':'Could you please explain MiTM?'}]
-    prompt_text = "Could you please explain MiTM?"
+    prompt_text = "Could you please explain a Man in the middle attack in detail?"
                      #'system', 'content' : 'You are a Cybersecurity Expert, with many years of experience in Cybersecurity. You are a friendly, professional, and have an eye for detail. You are an expert at vulnerability assessment report generation, where you convey the results of network vulnerability assessments, web application vulnerability assessments and malicious file scanning in a friendly, accurate and as non-technical as possible. We would like to be approachable to a wide variety of audiences, thus this is the reason behind this instruction.'}]
 
-    answer = generation_api.generate(prompt=prompt_text,options=dict(temperature=float(0.7)),raw=False,system='You are a Cybersecurity Expert with many years of experience in Cybersecurity. You are a friendly professional and have an eye for detail.You are an expert at vulnerability assessment report generation where you convey the results of network vulnerability assessments web application vulnerability assessments and malicious file scanning in a friendly accurate and as non-technical as possible. We would like to be approachable to a wide variety of audiences thus this is the reason behind this instruction.',format="json")
+#   ... Now , we generate the response :
+    try:
+        answer = generation_api.generate(prompt=prompt_text,options=dict(temperature=float(0.7)),system=system_instruction,format="json")
          #                               ,context=
          #                               ,total_duration=
          #                               ,load_duration=
@@ -46,11 +56,14 @@ def the_ollama_generator(): #nmap_report,nikto_report,clamav_report,wapiti_repor
          #                               ,eval_count=
          #                               ,eval_duration=
          # )
-    prompt_response = answer.response
-    return prompt_response
+
+        prompt_response = answer.response
+        return prompt_response
+    except Exception as e:
+        print(f"Error In Response Generation : {e}")
 
 
-the_ollama_generator()
+print(the_ollama_generator())
 
 
 
