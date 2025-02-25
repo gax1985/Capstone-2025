8956d3d90fb6a247bba6c59df9b53de61cece972
gax1985 <gax1985@gmail.com>
2025-02-25 09:32:26 -0400

    Managed to get a response from the Large Language Model

--- Python Files/Ollama_avec_LLM-AXE.py
+++ Python Files/Ollama_avec_LLM-AXE.py
@@ -1,9 +1,8 @@
-#Let us start by importing the much needed Ollama-Python library :
-import ollama
+# Let us start by importing the much needed Ollama-Python library :
 import ollama_python
+import llm_axe
 import beautifulsoup4
 from ollama_python.endpoints import ModelManagementAPI
-
 
 # We can declare the Ollama Model Management API's endpoint, so we can start with issuing commands to get the model :
 
@@ -13,38 +12,40 @@ ModelManagerAPI = ollama_python.endpoints.ModelManagementAPI(base_url="http://lo
 # ... next, let us create a function to pull the desired model :
 
 def ollama_model_gogetter(modelname):
-
-#  ... let us use it to pull the model from the Ollama-Python library:
+    #  ... let us use it to pull the model from the Ollama-Python library:
 
     try:
         TheGetter = ModelManagerAPI.pull(modelname)
         print(f"Model {modelname} has been downloaded successfully!")
     except Exception as e:
         print(f"Error : {e}")
-# Debugging Statements
+    # Debugging Statements
     print(TheGetter.status)
     ModelManagerAPI.list_local_models()
 
+
 # Let us download a Cybersecurity-based LLM :
 
-#ollama_model_gogetter("ALIENTELLIGENCE/cybersecuritythreatanalysisv2")
+# ollama_model_gogetter("ALIENTELLIGENCE/cybersecuritythreatanalysisv2")
 
 
 #######################################################################################################################################
 
 
-def the_ollama_generator(): #(nmap_report, nikto_report, clamav_report, wapiti_report):
+def the_ollama_generator():  # (nmap_report, nikto_report, clamav_report, wapiti_report):
 
-#   What we need to do here is to use the GenerateAPI library, where we indicate Ollama's API Endpoint, and we select our LLM :
-    
-    from ollama_python import endpoints
-    generation_api = ollama_python.endpoints.GenerateAPI(model="ALIENTELLIGENCE/cybersecuritythreatanalysisv2",base_url="http://127.0.0.1:11434/api")
-    #generation_api = ollama_python.endpoints.GenerateAPI(model="ALIENTELLIGENCE/cybersecuritythreatanalysisv2",base_url="http://127.0.0.1:12345/api")
+    #   What we need to do here is to use the GenerateAPI library, where we indicate Ollama's API Endpoint, and we select our LLM :
 
+    #from ollama_python import endpoints
+    #generation_api = ollama_python.endpoints.GenerateAPI(model="ALIENTELLIGENCE/cybersecuritythreatanalysisv2",base_url="http://127.0.0.1:11434/api")
 
 
-#   Since we get a prompt response successfully from the previous test prompt, let us store an example nmap report : 
-    
+    from llm_axe import OllamaChat
+    llm = OllamaChat(host="http://127.0.0.1:11434/api",model="ALIENTELLIGENCE/cybersecuritythreatanalysisv2")
+    # generation_api = ollama_python.endpoints.GenerateAPI(model="ALIENTELLIGENCE/cybersecuritythreatanalysisv2",base_url="http://127.0.0.1:12345/api")
+
+    #   Since we get a prompt response successfully from the previous test prompt, let us store an example nmap report :
+
     nmap_report = """Starting Nmap 7.94SVN ( https://nmap.org/ ) at 2025-02-11 14:47 AST
 
 Nmap scan report for scanme.nmap.org (45.33.32.156)
@@ -115,12 +116,9 @@ End Date:   2025:02:18 11:46:43"""
 + GET /images/: Directory indexing found.
 + GET /icons/README: Apache default file found. See: https://www.vntweb.co.uk/apache-restricting-access-to-iconsreadme/: """
 
-
-    with open("wapiti_report.html", "r") as f:
-        extracted_report = beautifulsoup4(f,'html.parser')
-        extracted_report
-
-
+    # with open("wapiti_report.html", "r") as f:
+    #     extracted_report = beautifulsoup4(f, 'html.parser')
+    #     extracted_report
 
     prompt_text = f""""We have ran a series of network, web-application and malware scans of the user's system. Please generate a clear, concise and user-friendly concise report
     indicating the vulnerabilities found, each vulnerability's rating (0-10 from 0 being harmless , to 10 being catastrophic). Afterwards, 
@@ -130,29 +128,39 @@ End Date:   2025:02:18 11:46:43"""
     I will include the reports now : {nmap_report} , {wapiti_report} , {clamav_report} and {nikto_report}"""
     ##Here is the report {nmap_report}"""
 
-
-#   Let us add a variable and store the system prompt in it :
-
+    #   Let us add a variable and store the system prompt in it :
 
     system_instruction = "You are a Cybersecurity Expert with many years of experience in Cybersecurity. You are a friendly professional and have an eye for detail. You are an expert at vulnerability assessment report generation where you convey the results of network vulnerability assessments, web application vulnerability assessments and malicious file scanning in a friendly, accurate and as non-technical as possible. We would like to be approachable to a wide variety of audiences, thus this is the reason behind this instruction."
 
-#   prompt_text = "Could you please explain a Man in the middle attack in detail?"
+    #   prompt_text = "Could you please explain a Man in the middle attack in detail?"
 
-#   ... Now , we generate the response :
+    #   ... Now , we generate the response :
     try:
-        answer = generation_api.generate(prompt=prompt_text,options=dict(temperature=float(0.3)),system=system_instruction)#,format="json")
-         #                               ,context=
-         #                               ,total_duration=
-         #                               ,load_duration=
-         #                               ,prompt_eval_duration=
-         #                               ,eval_count=
-         #                               ,eval_duration=
-         # )
+        # answer = generation_api.generate(prompt=prompt_text, options=dict(temperature=float(0.3)),
+        #                                  system=system_instruction)
+        #                                ,format="json")
+        #                               ,context=
+        #                               ,total_duration=
+        #                               ,load_duration=
+        #                               ,prompt_eval_duration=
+        #                               ,eval_count=
+        #                               ,eval_duration=
+        # )
+        prompts_of_LLM = []
+        prompts.append(prompt_text)
+        answer = OllamaChat.ask(llm,prompts=prompts_of_LLM,temperature=0.3,stream=True)
 
-        prompt_response = answer.response
+        Try:
+            for character in answer:
+                print(character)
+        Except:
+                print("OOPs! Something is up with our brainy assistant!")
+
+        #prompt_response = answer.response
+        
         return prompt_response
 
-        
+
     except Exception as e:
         print(f"Error In Response Generation : {e}")
 
